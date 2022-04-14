af60d95ad03d81448f9a64e87a68850f2700e7f2
“[Martialeagle <k.l.ashwanthram@gmail.com>
2022-04-14 17:39:12 +0530

    final project

---
+++ scripts/deploy.js
@@ -0,0 +1,23 @@
+const main = async () => {
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
+
+  await waveContract.deployed();
+
+  console.log("WavePortal address: ", waveContract.address);
+};
+
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+  
+  runMain();
